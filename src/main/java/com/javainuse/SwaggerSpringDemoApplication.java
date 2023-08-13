500be221852a2a8d468ffcdbbe77a96dda1dff02
Sai kotha <saikumar.kotha@gmail.com>
2023-08-13 15:25:51 +0530

    Updated

--- src/main/java/com/javainuse/SwaggerSpringDemoApplication.java
+++ src/main/java/com/javainuse/SwaggerSpringDemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SwaggerSpringDemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { //Java 
 		SpringApplication.run(SwaggerSpringDemoApplication.class, args);
 	}
 
